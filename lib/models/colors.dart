8dae1a27a8e3a4fcac9a94e29d881c9e1aebd7c8
Vitaminn171 <lyquocan171@gmail.com>
2024-10-16 00:41:42 +0700

    update optimize some code

--- lib/models/colors.dart
+++ lib/models/colors.dart
@@ -1,37 +1,37 @@
 import 'dart:ui';
 
-final primaryColor = Color(0xFFc3cc9c);
-final secondaryColor = Color(0xFF689374);
-final tertiaryColor = Color(0xFFF4F4F4);
-final alternateColor = Color(0xFFD6D6D6);
-final alternate1Color = Color(0xFFEDEDED);
-final alternate2Color = Color(0xFFA8A8A8);
+const primaryColor = Color(0xFFc3cc9c);
+const secondaryColor = Color(0xFF689374);
+const tertiaryColor = Color(0xFFF4F4F4);
+const alternateColor = Color(0xFFD6D6D6);
+const alternate1Color = Color(0xFFEDEDED);
+const alternate2Color = Color(0xFFA8A8A8);
 
-final textPrimary = Color(0xFF444444);
-final textSecondary = Color(0xFF57636C);
+const textPrimary = Color(0xFF444444);
+const textSecondary = Color(0xFF57636C);
 
-final accentColor_1 = Color(0xFFF4F4F4);
-final accentColor_2 = Color(0xFFF4F4F4);
-final accentColor_3 = Color(0xFFF4F4F4);
-final accentColor_4 = Color(0xCCFFFFFF);
+const accentColor_1 = Color(0xFFF4F4F4);
+const accentColor_2 = Color(0xFFF4F4F4);
+const accentColor_3 = Color(0xFFF4F4F4);
+const accentColor_4 = Color(0xCCFFFFFF);
 
-final success = Color(0xFF249689);
-final error= Color(0xFFFF5963);
-final warning = Color(0xFFF9CF58);
-final info = Color(0xFFffffff);
+const success = Color(0xFF249689);
+const error= Color(0xFFFF5963);
+const warning = Color(0xFFF9CF58);
+const info = Color(0xFFffffff);
 
-final successBackground = Color(0xFFc6f2d9);
-final successText = Color(0xFF20724f);
+const successBackground = Color(0xFFc6f2d9);
+const successText = Color(0xFF20724f);
 
-final errorBackground = Color(0xFFfecad3);
-final errorText = Color(0xFF8e434f);
+const errorBackground = Color(0xFFfecad3);
+const errorText = Color(0xFF8e434f);
 
-final shadow = Color(0x37333333);
-final backgroundColor = Color(0xFFF4F4F4);
+const shadow = Color(0x37333333);
+const backgroundColor = Color(0xFFF4F4F4);
 
-final tag0 = Color(0xFFC96868);
-final tag1 = Color(0xFFE3C784);
-final tag2 = Color(0xFF689374);
-final tag3 = Color(0xFF7EACB5);
+const tag0 = Color(0xFFC96868);
+const tag1 = Color(0xFFE3C784);
+const tag2 = Color(0xFF689374);
+const tag3 = Color(0xFF7EACB5);
 
-final glassColor = Color(0x6EF4F4F4);
+const glassColor = Color(0x6EF4F4F4);
